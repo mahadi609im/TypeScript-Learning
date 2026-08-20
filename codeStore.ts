24c98c33afec2a1e72af0b490e8665fc1c06d5ce
mahadi609im <maha609im@gmail.com>
2026-08-20 11:11:41 +0600

    learn TypeScript rest and spread operators

--- codeStore.ts
+++ codeStore.ts
@@ -113,3 +113,21 @@ const getFoodsPrice = (food: Foods) => {
 };
 
 console.log(getFoodsPrice(foods));
+
+// rest Operator vs spread Operator
+
+const arr1: number[] = [2, 4, 6, 8, 10];
+
+const arr2: number[] = [...arr1, 1, 3, 5, 7, 9];
+// const arr3: string[] = [...arr1, 1, 3, 5, 7, 9]; //Error
+
+console.log(arr2);
+
+const nums: number[] = [2, 4, 6, 8, 10];
+
+const restFunc = (...numRest: number[]) => {
+  const result = numRest.reduce((a, b) => a + b);
+  return result;
+};
+
+console.log(restFunc(...nums));
