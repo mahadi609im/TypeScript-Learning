e4dac2fdea4d1d44a8cc73e7c900125ba449641e
mahadi609im <maha609im@gmail.com>
2026-08-20 09:21:01 +0600

    primitive/non primitive data

---
+++ codeStore.ts
@@ -0,0 +1,16 @@
+// Primitive type
+const studentName: string = 'Mahadi';
+const roll: number = 340;
+const isPresent: boolean = true;
+const coupon: null = null;
+const deliveryCharge: undefined = undefined;
+
+// Non-Primitive type
+const foodName: string[] = ['one', 'two', 'three'];
+const rollNum: number[] = [34, 34, 53, 55];
+
+foodName.push('four');
+
+// Tuple
+const studentLocation: [number, number] = [33.53, 43.533];
+const studentData: [string, number] = ['Mahadi', 30];
